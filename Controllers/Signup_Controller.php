691ee541f078946bce348e648b6303388d0c9ca0
agra abderrahim <abder.agra@gmail.com>
2022-06-15 01:10:21 +0100

    the sign up model verified

--- Controllers/Signup_Controller.php
+++ Controllers/Signup_Controller.php
@@ -1,8 +1,35 @@
 <?php
 
 namespace Controllers;
+use database\Connection;
+use Models\Signup_Model;
+use app\Redirect;
 
 class Signup_Controller
 {
+        public function signup(){
+            if(isset($_POST['submit']))
+            {
+
+                $data= array(
+
+                    'firstname'=>$_POST['firstname'],
+                    'lastname'=>$_POST['lastname'],
+                    'email'=>$_POST['email'],
+                    'phone'=>$_POST['phone'],
+                    'password'=>$_POST['password'],
+                );
+                $signup = Signup_Model::Sign_up($data);
+                if($signup=='ok'){
+
+                echo 'sigu up successfully';
+                }else
+                {
+                    echo 'Please check your Data';
+                }
+            }
+
+
+        }
 
 }
